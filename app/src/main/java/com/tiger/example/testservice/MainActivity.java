72c8a250757c8721e42e7ad7abe58bffe97e702f
yangyin <yangyin@inad.com>
2016-10-27 10:07:04 +0800

    活动和服务进行通信

--- app/src/main/java/com/tiger/example/testservice/MainActivity.java
+++ app/src/main/java/com/tiger/example/testservice/MainActivity.java
@@ -1,23 +1,46 @@
 package com.tiger.example.testservice;
 
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String TAG = "MainActivity";
     private Button startServiceButton;
     private Button stopServiceButton;
     private Context context;
+    private Button bindServiceButton;
+    private Button unbindServiceButton;
+    private MyServiece.DownlaodBinder downlaodBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            downlaodBinder = (MyServiece.DownlaodBinder) iBinder;
+            downlaodBinder.startDownload();
+            downlaodBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            Log.d(TAG, "onServiceDisconnected: executed");
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startServiceButton = (Button) findViewById(R.id.start_service);
         stopServiceButton = (Button) findViewById(R.id.stop_service);
+        bindServiceButton = (Button) findViewById(R.id.bind_service);
+        unbindServiceButton = (Button) findViewById(R.id.unbind_service);
         context = this.getApplicationContext();
         startServiceButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +54,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent stopIntent = new Intent(context, MyServiece.class);
                 stopService(stopIntent);
+            }
+        });
+        bindServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent bindIntent = new Intent(context, MyServiece.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);
+            }
+        });
+        unbindServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                unbindService(connection);
             }
         });
     }
--- app/src/main/java/com/tiger/example/testservice/MyServiece.java
+++ app/src/main/java/com/tiger/example/testservice/MyServiece.java
@@ -2,6 +2,7 @@ package com.tiger.example.testservice;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
@@ -13,11 +14,12 @@ import android.util.Log;
 public class MyServiece extends Service {
 
     private static String TAG = "MyService";
+    private DownlaodBinder mBinder = new DownlaodBinder();
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
     }
 
     @Override
@@ -36,5 +38,17 @@ public class MyServiece extends Service {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: executed");
+    }
+
+    class DownlaodBinder extends Binder {
+
+        public void startDownload() {
+            Log.d(TAG, "startDownload: executed");
+        }
+
+        public int getProgress() {
+            Log.d(TAG, "getProgress: executed");
+            return 0;
+        }
     }
 }
